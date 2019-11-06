717fa5eed96bd0c51908e503983ceaccb25a4333
Marhadiasha Kusumawardhana <marhadiasak@icloud.com>
2019-11-07 00:49:49 +1100

    column order switching

--- models/Usage.py
+++ models/Usage.py
@@ -7,14 +7,14 @@ class Usage(Base):
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey("users.id"))
     ip_address = Column(String)
-    status_code = Column(Integer)
     path = Column(String)
+    status_code = Column(Integer)
     used_at = Column(DateTime)
 
-    def __init__(self, id, user_id, path, used_at, ip_address, status_code):
+    def __init__(self, id, user_id, ip_address, path, status_code, used_at):
         self.id = id
         self.user_id = user_id
-        self.path = path
-        self.used_at = used_at
         self.ip_address = ip_address
+        self.path = path
         self.status_code = status_code
+        self.used_at = used_at
