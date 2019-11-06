880b4247699d468228695a06d3748badfef28523
Marhadiasha Kusumawardhana <marhadiasak@icloud.com>
2019-11-07 00:52:20 +1100

    add price_range to constructor

--- models/Property.py
+++ models/Property.py
@@ -13,10 +13,11 @@ class Property(Base):
     price_range = Column(String)
 
     def __init__(self, id, zip_code, property_type, room_type, guests_count,
-                 bed_count):
+                 bed_count, price_range):
         self.id = id
         self.zip_code = zip_code
         self.property_type = property_type
         self.room_type = room_type
         self.guest_count = guests_count
         self.bed_count = bed_count
+        self.price_range = price_range
