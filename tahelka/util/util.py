f190d7a27551acdf735b31d073b0cb2f5247734c
yashtamakuwala <neel64tamakuwala@gmail.com>
2019-11-10 00:58:22 +1100

    Shortened code to check for empty fields

--- tahelka/util/util.py
+++ tahelka/util/util.py
@@ -1,8 +1,4 @@
 # Checks if all fields have values, True when all are None
 def areFieldsEmpty(*argv):
-    params = list()
-    for arg in argv:
-        params.append(arg)
-
-    if all(p is None for p in params):
+    if all(p is None for p in argv):
         return True
