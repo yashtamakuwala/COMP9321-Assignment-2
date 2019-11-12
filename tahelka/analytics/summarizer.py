393515d89c922282beb7e6a31dc225e517c6a00e
Marhadiasha Kusumawardhana <marhadiasak@icloud.com>
2019-11-12 13:14:23 +1100

    wrong

--- tahelka/analytics/summarizer.py
+++ tahelka/analytics/summarizer.py
@@ -1,9 +1,8 @@
 class Summarizer:
-    def __init__(self, user_id, ip_address, action, status_code):
+    def __init__(self, date_start=None, date_end=None, user_id=None):
+        self.date_start = date_start
+        self.date_end = date_end
         self.user_id = user_id
-        self.ip_address = ip_address
-        self.action = action
-        self.status_code = status_code
 
     def summarize(self):
         return {"summary": "the summary"}
