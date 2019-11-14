8c81556cca407fa2bc80cf32b317b22cbfe0165f
Marhadiasha Kusumawardhana <marhadiasak@icloud.com>
2019-11-14 22:07:05 +1100

    put to buckets

--- scripts/clean_listing.py
+++ scripts/clean_listing.py
@@ -4,6 +4,21 @@ import pandas as pd
 def normalize_price(price_raw):
     return int(float(price_raw.replace(',','').replace('$','')))
 
+def decide_bucket(price):
+    buckets = [40, 50, 60, 70, 80, 90, 100, 120, 130, 145, 160, 190, 210, 250,
+               300, 500]
+
+    if price <= buckets[0]:
+        return f'<= ${buckets[0]}'
+
+    for index, bucket in enumerate(buckets):
+        if price > bucket:
+            continue
+
+        return f'${buckets[index - 1]} - ${bucket}'
+
+    return f'> ${buckets[-1]}'
+
 root = os.path.abspath(os.curdir)
 listing_path = os.path.join(root, 'data/listings.csv')
 clean_crime_path = os.path.join(root, 'data/crime_cleaned.csv')
@@ -32,6 +47,9 @@ df = df.astype({'beds' : int})
 
 # Normalize price
 df['price'] = df['price'].apply(normalize_price)
+
+# Put price to buckets
+df['price_range'] = df['price'].apply(decide_bucket)
 
 print(df)
 print(len(df))
--- scripts/explore_price_buckets.py
+++ scripts/explore_price_buckets.py
@@ -1,25 +1,24 @@
 import os
 import pandas as pd
 
-def get_bucket(price):
-    if price >= 500:
-        return ">= $500"
-    if 300 <= price and price < 500:
-        return "$300 - $500"
-    if 200 <= price and price < 300:
-        return "$200 - $300"
-    if price < 40:
-        return "< $40"
+def decide_bucket(price):
+    buckets = [45, 50, 60, 70, 80, 90, 100, 120, 130, 145, 160, 190, 210, 250,
+               300, 500]
+    if price <= buckets[0]:
+        return f'<= ${buckets[0]}'
 
-    return f"${price // 20 * 20} - ${price // 20 * 20 + 20}"
+    for index, bucket in enumerate(buckets):
+        if price > bucket:
+            continue
 
-os.chdir("..")
-os.chdir("..")
+        return f'${buckets[index - 1]} - ${bucket}'
+
+    return f'> ${buckets[-1]}'
+
 root = os.path.abspath(os.curdir)
 path_of_data = os.path.join(root, 'data/listings_cleaned.csv')
 df = pd.read_csv(path_of_data)
 
 df = df.sort_values('price')
-df['bucket'] = df['price'].apply(get_bucket)
+df['bucket'] = df['price'].apply(decide_bucket)
 print(df['bucket'].value_counts()[:50])
-
