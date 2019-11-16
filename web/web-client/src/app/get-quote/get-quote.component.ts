678446c44513ea7d53dd03757c24bc328b6585bc
jatin7gupta <guptajatin@outlook.com>
2019-11-16 22:32:46 +1100

    added LGA  in quote

--- web/web-client/src/app/get-quote/get-quote.component.ts
+++ web/web-client/src/app/get-quote/get-quote.component.ts
@@ -17,11 +17,73 @@ import {WebMethodsService} from '../services/web-methods.service';
 })
 export class GetQuoteComponent implements OnInit {
   myform: FormGroup;
+  LGAArray: Array<string> = [
+    'Blacktown',
+    'Botany Bay',
+    'Burwood',
+    'Camden',
+    'Campbelltown',
+    'Canada Bay',
+    'Canterbury-Bankstown',
+    'Cumberland',
+    'Fairfield',
+    'Georges River',
+    'Hornsby',
+    'Hunters Hill',
+    'Inner West',
+    'Ku-ring-gai',
+    'Lane Cove',
+    'Liverpool',
+    'Mosman',
+    'North Sydney',
+    'Northern Beaches',
+    'Parramatta',
+    'Penrith',
+    'Randwick',
+    'Rockdale',
+    'Ryde',
+    'Strathfield',
+    'Sutherland Shire',
+    'Sydney',
+    'The Hills Shire',
+    'Waverley',
+    'Willoughby',
+    'Woollahra'
+  ];
+  propertyTypeArray: Array<string> = [
+    'Apartment',
+    'House',
+    'Townhouse',
+    'Condominium',
+    'Guest suite',
+    'Guesthouse',
+    'Villa',
+    'Serviced apartment',
+    'Loft',
+    'Bungalow',
+    'Boutique hotel',
+    'Bed and breakfast',
+    'Cottage',
+    'Hostel',
+    'Cabin',
+    'Other',
+    'Hotel',
+    'Tiny house',
+    'Boat',
+    'Camper/RV',
+    'Aparthotel'
+  ];
+  roomTypeArray: Array<string> = [
+    'Entire home/apt',
+    'Private room',
+    'Shared room',
+    'Hotel room'
+  ];
   constructor(private router: Router, private authenticationService: AuthenticationService, private webService: WebMethodsService) { }
 
   ngOnInit() {
     this.myform = new FormGroup({
-      zipCode: new FormControl(),
+      LGA: new FormControl(),
       propertyType: new FormControl(),
       roomType: new FormControl(),
       guestCount: new FormControl(),
@@ -31,7 +93,7 @@ export class GetQuoteComponent implements OnInit {
   getQuote(event) {
     event.preventDefault();
     const quote = new Quote(
-      this.myform.value.zipCode, this.myform.value.propertyType,
+      this.myform.value.LGA, this.myform.value.propertyType,
       this.myform.value.roomType, this.myform.value.guestCount,
       this.myform.value.bedCount);
     this.webService.getQuote(quote).subscribe(success => {
