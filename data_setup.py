fda57dbd69904d4710400ec9a0be80dc9d2a9d45
yashtamakuwala <neel64tamakuwala@gmail.com>
2019-11-17 20:08:05 +1100

    creating admin user while setting up DB.

--- data_setup.py
+++ data_setup.py
@@ -3,3 +3,18 @@ from scripts import clean_unemployment
 from scripts import clean_lga
 from scripts import clean_listing
 from scripts import dump_to_db
+
+from alchemy import Session
+from tahelka.models.User import User
+from tahelka.auth.hash_generator import HashGenerator
+
+def createAdmin():
+    session = Session()
+    password = 'admin'
+    hashed_password = HashGenerator(password).generate()
+    admin = User('admin','admin','admin', hashed_password, True)
+
+    session.add(admin)
+    session.commit()
+
+createAdmin()
