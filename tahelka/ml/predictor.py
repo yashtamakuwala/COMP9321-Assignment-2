13f5403d385fc3e50ddaa26fc181952b723c7950
atulmkamat <atulmkamat@gmail.com>
2019-11-17 15:25:27 +1100

    Trainer model finalized

--- tahelka/ml/predictor.py
+++ tahelka/ml/predictor.py
@@ -21,14 +21,8 @@ class Predictor:
         le_room_type = LabelEncoder()
         le_zipcode = LabelEncoder()
         # The encoding for property_type
-        le_property_type.fit(['Aparthotel', 'Apartment', 'Barn', 'Bed and breakfast', 'Boat', 
-        'Boutique hotel', 'Bungalow', 'Cabin', 'Camper/RV', 'Campsite', 'Casa particular (Cuba)', 
-        'Castle', 'Cave', 'Chalet', 'Condominium', 'Cottage', 'Dome house', 'Earth house', 'Farm stay', 
-        'Guest suite', 'Guesthouse', 'Heritage hotel (India)', 'Hostel', 'Hotel', 'House', 
-        'Island', 'Loft', 'Minsu (Taiwan)', 'Other', 'Resort', 'Serviced apartment', 
-        'Tent', 'Tiny house', 'Tipi', 'Townhouse', 'Train', 'Treehouse', 'Villa', 'Yurt'])
+
         #encoding for room_type
-        le_room_type.fit(['Entire home/apt', 'Hotel room', 'Private room', 'Shared room'])
 
         #stored model for zip code
         zip_code_file = open('zipcode_encoder.pkl', 'rb')
@@ -39,9 +33,9 @@ class Predictor:
         with open(filename, 'rb') as file:  
             loaded_model = pickle.load(file)
         #X axis would have columns "property_type","accommodates","beds","room_type","zipcode"
-        self.property_type = le_property_type.transform(self.property_type)
-        self.room_type = le_room_type.transform(self.room_type)
-        self.zip_code = le_zipcode.transform(self.zip_code)
+        self.property_type = le_property_type.transform([self.property_type])[0]
+        self.room_type = le_room_type.transform([self.room_type])[0]
+        
         X_test = [[self.property_type,self.guest_count,self.bed_count,self.room_type,self.zip_code]]
         predicted_result = loaded_model.predict(X_test)
         print(predicted_result)     
--- tahelka/ml/trainer.py
+++ tahelka/ml/trainer.py
@@ -16,187 +16,46 @@ from sklearn.utils import shuffle
 from sklearn.metrics import precision_score, accuracy_score, recall_score
 
 class Trainer:
-    '''PRICE_BUCKETS = [-1, 40, 50, 60, 70, 80, 90, 100, 115, 125, 140, 150, 175, 200, 210,
-                     250, 300, 400, 600, 100_000]
-    '''
     PRICE_BUCKETS = [-1, 40, 60, 80, 100, 125, 150, 200, 250, 300, 500, 99_999]
     CRIME_BUCKETS = [-1, 250, 500, 1000, 1500, 100_000]
     UNEMP_BUCKETS = [-1, 2, 4, 6, 8, 100_000]
 
     def train(self):
-        df = self.prepare_ml_df()
+        df = self.prepare_ml_df()   
+
+        # Encode the categorical attributes
         le_property_type = LabelEncoder()
         le_room_type = LabelEncoder()
         df['property_type'] = le_property_type.fit_transform(df['property_type'])
         df['room_type'] = le_room_type.fit_transform(df['room_type'])
-        #print(le_property_type.classes_)
-        #np.save('classes.npy', le_property_type.classes_)
-        Xaxis = df.values[:, [0,1,2,3,5,6]]
-        #print(df)
-        # Y axis would have price bins
-        Yaxis = df.values[:,4]
-        Xaxis =Xaxis.astype('int')
-        Yaxis = Yaxis.astype('int')
-        X_train, X_test, y_train, y_test = (
-            train_test_split(Xaxis, Yaxis, test_size=0.33, random_state=100
-        )
 
-        #Gini impurity an entropy are what are called selection criterion for decision trees.
+        # Get X & Y
+        Xaxis = df.values[:, [0,1,2,3,5,6]].astype('int')
+        Yaxis = df.values[:,4].astype('int')
 
-        ### GINI ###
-        gini = DecisionTreeClassifier(criterion = "gini",
-                random_state = 100,max_depth=3, min_samples_leaf=5)
-        gini.fit(X_train, y_train)
-        ### ENTROPY ###
-        entropy = DecisionTreeClassifier(
-                criterion = "entropy", random_state = 100,
-                max_depth = 3, min_samples_leaf = 5)
-        entropy.fit(X_train, y_train)
-        ### MODEL CREATION  ####
-        y_pred = gini.predict(X_test)
-        entropy_pred = entropy.predict(X_test)
+        # Create the model
+        gini = DecisionTreeClassifier(criterion="gini",
+                                      random_state = 100, max_depth=3, min_samples_leaf=5)
+        gini.fit(Xaxis, Yaxis)
 
-        errors = 0
-        count = 0
+        # Save gini model using pickle
+        with open('ml_models/model.sav', 'wb') as file:
+            pickle.dump(gini, file)
 
-        for index, pred in enumerate(y_pred):
-            #print(X_test[index], y_test[index], pred)
-            errors += abs(pred - y_test[index])
-            count += 1
+        file_for_prop = open('ml_models/property_type_encoding.sav', 'wb')
+        file_for_room = open('ml_models/room_type_encoding.sav', 'wb')
+        pickle.dump(le_property_type,file_for_prop)
+        pickle.dump(le_room_type,file_for_room)
 
-        print(f'mean error: {errors / len(y_pred)}')
-
-
-        filename = 'price_prediction_model.sav'
-
-        #consider the model with higher accuracy
-        print(accuracy_score(y_test,y_pred)*100)
-        print(accuracy_score(y_test,entropy_pred)*100)
-
-        if ((accuracy_score(y_test,y_pred)*100) >= (accuracy_score(y_test,entropy_pred)*100) ):
-            with open(filename, 'wb') as file:
-                pickle.dump(gini, file)
-        else:
-            with open(filename, 'wb') as file:
-                pickle.dump(entropy, file)
-
-
-
-        '''
-        os.chdir("..")
-        os.chdir("..")
-        root = os.path.abspath(os.curdir)
-        path_of_data = os.path.join(root, 'data/listings.csv')
-        path_of_data2 = os.path.join(root, 'data/RCI_OffenceByMonth2.csv')
-        df = pd.read_csv(path_of_data)
-        dfc = pd.read_csv(path_of_data2)
-
-        required_columns = ["latitude","longitude", "bathrooms","bedrooms",
-                    "square_feet","minimum_nights","maximum_nights","price","weekly_price","monthly_price","beds"]
-        # the values of these columns would be converted to int
-        integer_columns = ["bathrooms","bedrooms","minimum_nights","maximum_nights","beds"]
-        # few more columns thar are strings
-        additional_columns = ["accommodates","property_type","bed_type","zipcode","room_type","city"]
-        for i in integer_columns:
-            df[i] = df[i].fillna(int(-1))
-        df3 = df[required_columns]
-        # copy df3 to df3
-        df4 = df3.copy()
-        df4 = convert_to_type_and_clean(df, df3, df4, integer_columns,
-                            required_columns, additional_columns)
-        #a = df4["city"].unique()
-        #print(sorted(a)
-        #dfc = lower_and_remove_special_char(dfc,"LGA")
-        dfc['city'] = dfc['LGA']
-        range_of_col = [i for i in range(4,220)]
-        #df4 = df4.merge(dfc,on="city", how = "left")
-        #dfc = dfc[pd.notnull(dfc['LGA'])]
-        print(df4['city'].value_counts().to_dict())
-        #print(df4['city'].unique())
-        #print(dfc['LGA'].isna().sum())
-
-        print(dfc.columns)
-
-        The bins are decided here
-        The labels are one less than the size of bins
-
-        bins = [-0.1,10,70,200,500,2000,5000, 10000]
-        labels = [i for i in range(1,len(bins))]
-
-        #add another column price_bin
-        df4['price_bin'] = pd.cut(df4['price'], bins=bins, labels=labels)
-
-        #The main df on which we creaTE OUR ML MODEL
-
-        #THe label encoder which does the preprocessing
-        #where categorical data is converted to numerical.
-
-        le_property_type = LabelEncoder()
-        le_room_type = LabelEncoder()
-        le_zipcode = LabelEncoder()
-
-        #the 3 columns are updated with the preprocessed values
-
-        #print(df4['property_type'].iloc[:5])
-
-        df4['property_type'] = le_property_type.fit_transform(df4['property_type'])
-        df4['room_type'] = le_room_type.fit_transform(df4['room_type'])
-        df4['zipcode'] = df4['zipcode'].apply(convert_to_str)
-        df4['zipcode'] = le_zipcode.fit_transform(df4['zipcode'])
-        #print(le_zipcode.classes_)
-        np.save('classes.npy', le_property_type.classes_)
-        #print(df4['property_type'].iloc[:5])
-
-        #X axis would have columns "property_type","accommodates","beds","room_type","zipcode"
-        Xaxis = df4.values[:, [0,1,2,3,4]]
-        # Y axis would have price bins
-        Yaxis = df4.values[:,6]
-        Xaxis =Xaxis.astype('int')
-        Yaxis = Yaxis.astype('int')
-        X_train, X_test, y_train, y_test = train_test_split(
-                Xaxis, Yaxis, test_size = 0.33, random_state = 100)
-        # predict the test set
-        #Gini impurity an entropy are what are called selection criterion for decision trees.
-
-        # the probability of a random sample being classified incorrectly.
-        gini = DecisionTreeClassifier(criterion = "gini",
-                random_state = 100,max_depth=3, min_samples_leaf=5)
-        gini.fit(X_train, y_train)
-        #-------------------------------------------------------------
-        #measurement of information
-        entropy = DecisionTreeClassifier(
-                criterion = "entropy", random_state = 100,
-                max_depth = 3, min_samples_leaf = 5)
-        entropy.fit(X_train, y_train)
-        #-------------------------------------------------------------
-        y_pred = gini.predict(X_test)
-        entropy_pred = entropy.predict(X_test)
-        # To store the Machine learning model- PICKLE is used
-        os.chdir("tahelka/ml")
-        filename = 'price_prediction_model.sav'
-        file_for_zipcode = open('zipcode_encoder.pkl', 'wb')
-        pickle.dump(le_zipcode,file_for_zipcode)
-        #consider the model with higher accuracy
-        if ((accuracy_score(y_test,y_pred)*100) >= (accuracy_score(y_test,entropy_pred)*100) ):
-            with open(filename, 'wb') as file:
-                pickle.dump(gini, file)
-        else:
-            with open(filename, 'wb') as file:
-                pickle.dump(entropy, file)
-        '''
     def prepare_ml_df(self):
         # Load dataframe
         df = pd.read_sql_table('properties', con=Engine)
-
         # Remove unneeded columns
         df = df.drop(['id','rating'], axis=1)
 
         # Put price into buckets
         df['price_range'] = pd.cut(df['price'], bins=Trainer.PRICE_BUCKETS,
                                    labels=range(1, len(Trainer.PRICE_BUCKETS)))
-
-        print(df['price_range'].value_counts())
-
         # Remove price column
         df = df.drop(['price'], axis=1)
 
@@ -214,6 +73,7 @@ class Trainer:
 
         # Drop lga
         df = df.drop(['lga'], axis=1)
+
         return df
 
     def prepare_crime_df(self):
