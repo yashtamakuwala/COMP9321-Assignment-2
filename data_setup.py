0664d11d1eff675b6bb8587cf98bcd3a3566031d
Marhadiasha Kusumawardhana <marhadiasak@icloud.com>
2019-11-17 22:59:16 +1100

    Fix dummy amdin email

--- data_setup.py
+++ data_setup.py
@@ -1,3 +1,4 @@
+# Run Data cleaning scripts & dump property dataset to db
 from scripts import clean_crime
 from scripts import clean_unemployment
 from scripts import clean_lga
@@ -9,14 +10,17 @@ from tahelka.models.User import User
 from tahelka.auth.hash_generator import HashGenerator
 from tahelka.ml.trainer import Trainer
 
+# Create an admin user
 def createAdmin():
     session = Session()
     password = 'admin'
     hashed_password = HashGenerator(password).generate()
-    admin = User('admin','admin','admin', hashed_password, True)
+    admin = User('admin','admin','admin@admin.com', hashed_password, True)
 
     session.add(admin)
     session.commit()
 
 createAdmin()
+
+# Train the model
 Trainer().train()
