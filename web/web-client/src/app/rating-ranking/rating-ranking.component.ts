2eeb6968e38cbba049b0e992388691f7a20c14da
jatin7gupta <guptajatin@outlook.com>
2019-11-17 21:45:02 +1100

    rating ranking completed

--- web/web-client/src/app/rating-ranking/rating-ranking.component.ts
+++ web/web-client/src/app/rating-ranking/rating-ranking.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
+import {WebMethodsService} from '../services/web-methods.service';
+import {AuthenticationService} from '../services/authentication.service';
+import {Router} from '@angular/router';
+import {RatingRankingResponse} from '../models/RatingRankingResponse';
 
 @Component({
   selector: 'app-rating-ranking',
@@ -7,9 +12,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RatingRankingComponent implements OnInit {
 
-  constructor() { }
+  myform: FormGroup;
+  orderArray: Array<string> = [
+    'ascending',
+    'descending',
+  ];
+  limitArray: Array<string> = [
+    '5',
+    '10',
+    '20',
+    'all'
+  ];
+  responseArray = Array<RatingRankingResponse>();
+  constructor(private webservice: WebMethodsService, private authenticationService: AuthenticationService, private router: Router) {
+    this.myform = new FormGroup({
+      order: new FormControl(),
+      limit: new FormControl()
+    });
+    this.myform.controls.order.setValue(this.orderArray[0], {onlySelf: true});
+    this.myform.controls.limit.setValue(this.limitArray[0], {onlySelf: true});
+  }
 
   ngOnInit() {
+  }
+  parseResponse(response: Array<RatingRankingResponse>) {
+    this.responseArray = response;
+  }
+  getRatingRanking(event) {
+    event.preventDefault();
+    this.webservice.getRanking(this.myform.value, 'rating_rankings').subscribe(success => {
+      this.parseResponse(success.data);
+    }, error => {
+      console.log(error);
+    });
+  }
+  logout() {
+    this.authenticationService.logout();
+    this.router.navigate(['/login']);
   }
 
 }
