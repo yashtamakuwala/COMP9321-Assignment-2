8a6f4927a4497f2b8e646564f082e1b60c3f4a0b
Marhadiasha Kusumawardhana <marhadiasak@icloud.com>
2019-11-18 23:02:20 +1100

    Remove rankers explorations cript

--- run_rankers.py
+++
@@ -1,9 +0,0 @@
-from tahelka.insight.price_ranker import PriceRanker
-from tahelka.insight.rating_ranker import RatingRanker
-from tahelka.insight.crime_ranker import CrimeRanker
-from tahelka.insight.unemployment_ranker import UnemploymentRanker
-
-print(PriceRanker(limit=10, ascending=True).rank())
-print(RatingRanker(limit=10, ascending=False).rank())
-print(CrimeRanker(limit=-1, ascending=False).rank())
-print(UnemploymentRanker(limit=20, ascending=True).rank())
