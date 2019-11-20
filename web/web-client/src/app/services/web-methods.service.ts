ff2bca601b7f5904e6368b94841e8f4c0427d889
jatin7gupta <guptajatin@outlook.com>
2019-11-20 20:41:05 +1100

    price ranking changed

--- web/web-client/src/app/services/web-methods.service.ts
+++ web/web-client/src/app/services/web-methods.service.ts
@@ -31,7 +31,7 @@ export class WebMethodsService {
   }
 
   getQuote(quote: Quote): Observable<any> {
-    const quoteUrl = this.API_URL + 'predictions';
+    const quoteUrl = this.API_URL + 'property_price_prediction';
     let headers_ = new HttpHeaders();
     headers_ = headers_.set('Authorization', 'Bearer ' + this.authenticationService.currentUserValue.token);
     const params_ = new HttpParams()
@@ -43,7 +43,8 @@ export class WebMethodsService {
     return this.http.get(quoteUrl, {params: params_, headers: headers_});
   }
   getRanking(priceRanking: PriceRanking, functionName: string): Observable<any> {
-    const queryUrl = this.API_URL + functionName;
+    const lga = this.API_URL + 'local_government_areas/';
+    const queryUrl = lga + functionName;
     let headers_ = new HttpHeaders();
     headers_ = headers_.set('Authorization', 'Bearer ' + this.authenticationService.currentUserValue.token);
     const params_ = new HttpParams()
